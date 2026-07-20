2f1f334f392f6419bf3a9d332801c6fa7d22778d
Deivy Batista <deivy.batista.ba@gmail.com>
2026-07-20 17:34:08 -0300

    Update conta.py and add conta_repository.py

--- src/models/conta.py
+++ src/models/conta.py
@@ -1,7 +1,16 @@
+from models.cliente import Cliente
+
 class Conta:
-    def __init__(self, numero: str, agencia:str, cliente):
+    def __init__(self, numero: str, agencia:str, cliente: Cliente):
         
         self.numero = numero
         self.agencia = agencia
         self.cliente = cliente
-    
+
+    def __str__(self):
+        return (
+            "=================================\n"
+            f"Conta: {self.numero}\n" 
+            f"Agência: {self.agencia}\n" 
+            "================================="
+        )
---
+++ src/repositories/conta_repository.py
@@ -0,0 +1,22 @@
+from models.conta import Conta
+from database.banco import contas
+
+class ContaRepository:
+
+    def create(conta: Conta):
+        conta.append(conta)
+
+    def find_all():
+        return contas
+
+    def find_by_nome(numero: str):
+        for conta in contas:
+            if conta.numero == numero:
+                return conta
+            
+        return None
+
+    def delete(numero:str):
+        for index, conta in enumerate(contas):
+            if conta.numero == numero:
+                contas.pop(index)
